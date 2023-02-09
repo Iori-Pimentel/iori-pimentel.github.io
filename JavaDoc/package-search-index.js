2073de6fe5157dddadf14243ba9201d59ab51774
Iori Pimentel <ioripimentel64@gmail.com>
2023-02-10 00:10:56 +0800

    Init Commit for Fraction JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"prog2.prelimgroup"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"prog2.prelimgroup","l":"Fraction"}];updateSearchResults();
